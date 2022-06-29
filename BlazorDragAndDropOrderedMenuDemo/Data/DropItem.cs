8a6e152a782448ea7942a32c9fa36c33da4141ed
germandefrancesco <germandefrancesco@hotmail.com>
2022-06-29 11:13:10 -0300

    fixed example from Mudblazor

---
+++ BlazorDragAndDropOrderedMenuDemo/Data/DropItem.cs
@@ -0,0 +1,8 @@
+﻿namespace BlazorDragAndDropOrderedMenuDemo.Data;
+
+public class DropItem
+{
+    public string Name { get; init; } = "";
+    public string Selector { get; set; } = "";
+    public int Order { get; set; }
+}
